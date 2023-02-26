d5c12a42db327c36c14fbeddc7d54eb7a8665e0a
Tatsuya Kubo <6iKezbAD3CZnf@gmail.com>
2023-02-26 08:03:31 +0000

    WIP

---
+++ field.cuh
@@ -0,0 +1,73 @@
+#define GOLDILOCKS_PRIME 0xffffffff0000001
+
+class F {
+public:
+    F() : p(GOLDILOCKS_PRIME), value(0) {}
+    F(unsigned long long v) : p(GOLDILOCKS_PRIME), value(v % p) {}
+
+    // Addition
+    F operator+(const F& other) const {
+            return F((value + other.value) % p);
+    }
+
+    // Subtraction
+    // F operator-(const F& other) const {
+    //         return F((value - other.value + p) % p);
+    // }
+
+    // Multiplication
+    // F operator*(const F& other) const {
+    //         return F((value * other.value) % p);
+    // }
+
+    // Division
+    // F operator/(const F& other) const {
+    //         unsigned long long inv = inverse_mod(other.value, p);
+    //         return F((value * inv) % p);
+    // }
+
+    // Exponentiation
+    // F pow(unsigned long long exp) const {
+    //     unsigned long long result = 1;
+    //     unsigned long long base = value;
+    //     while (exp > 0) {
+    //         if (exp % 2 == 1) {
+    //             result = (result * base) % p;
+    //         }
+    //         base = (base * base) % p;
+    //         exp /= 2;
+    //     }
+    //     return F(result);
+    // }
+
+    // Comparison
+    bool operator==(const F& other) const {
+        return value == other.value;
+    }
+    bool operator!=(const F& other) const {
+        return value != other.value;
+    }
+
+private:
+    unsigned long long p;
+    unsigned long long value;
+
+    // Computes the modular inverse of a modulo p using the extended Euclidean algorithm
+    // int inverse_mod(int a, int p) const {
+    //     int t = 0, new_t = 1;
+    //     int r = p, new_r = a;
+    //     while (new_r != 0) {
+    //         int quotient = r / new_r;
+    //         int tmp_t = new_t;
+    //         new_t = t - quotient * new_t;
+    //         t = tmp_t;
+    //         int tmp_r = new_r;
+    //         new_r = r - quotient * new_r;
+    //         r = tmp_r;
+    //     }
+    //     if (t < 0) {
+    //         t += p;
+    //     }
+    //     return t;
+    // }
+};
--- main.cu
+++ main.cu
@@ -1,98 +1,33 @@
+#include <array>
 #include <cassert>
 #include <vector>
 
+#include "field.cuh"
+
 #define SPONGE_WIDTH 12
-#define GOLDILOCKS_PRIME 0xffffffff0000001
 
-class F {
-public:
-    F() : p(GOLDILOCKS_PRIME), value(0) {}
-    F(unsigned long long v) : p(GOLDILOCKS_PRIME), value(v % p) {}
-
-    // Addition
-    F operator+(const F& other) const {
-            return F((value + other.value) % p);
+// Poseidon Hash
+std::array<F, SPONGE_WIDTH> poseidon(std::array<F, SPONGE_WIDTH> input) {
+    std::array<F, SPONGE_WIDTH> output;
+    for (int i=0; i<SPONGE_WIDTH; i++) {
+        output[i] = F(input[i]);
     }
-
-    // Subtraction
-    // F operator-(const F& other) const {
-    //         return F((value - other.value + p) % p);
-    // }
-
-    // Multiplication
-    // F operator*(const F& other) const {
-    //         return F((value * other.value) % p);
-    // }
-
-    // Division
-    // F operator/(const F& other) const {
-    //         unsigned long long inv = inverse_mod(other.value, p);
-    //         return F((value * inv) % p);
-    // }
-
-    // Exponentiation
-    // F pow(unsigned long long exp) const {
-    //     unsigned long long result = 1;
-    //     unsigned long long base = value;
-    //     while (exp > 0) {
-    //         if (exp % 2 == 1) {
-    //             result = (result * base) % p;
-    //         }
-    //         base = (base * base) % p;
-    //         exp /= 2;
-    //     }
-    //     return F(result);
-    // }
-
-    // Comparison
-    bool operator==(const F& other) const {
-        return value == other.value;
-    }
-    bool operator!=(const F& other) const {
-        return value != other.value;
-    }
-
-private:
-    unsigned long long p;
-    unsigned long long value;
-
-    // Computes the modular inverse of a modulo p using the extended Euclidean algorithm
-    // int inverse_mod(int a, int p) const {
-    //     int t = 0, new_t = 1;
-    //     int r = p, new_r = a;
-    //     while (new_r != 0) {
-    //         int quotient = r / new_r;
-    //         int tmp_t = new_t;
-    //         new_t = t - quotient * new_t;
-    //         t = tmp_t;
-    //         int tmp_r = new_r;
-    //         new_r = r - quotient * new_r;
-    //         r = tmp_r;
-    //     }
-    //     if (t < 0) {
-    //         t += p;
-    //     }
-    //     return t;
-    // }
-};
-
-F* poseidon(F* input) {
-    return input;
+    return output;
 }
 
-void check_test_vectors(std::vector<unsigned long long*> inputs, std::vector<unsigned long long*> expected_outputs) {
+void check_test_vectors(std::vector<std::array<unsigned long long, SPONGE_WIDTH>> inputs, std::vector<std::array<unsigned long long, SPONGE_WIDTH>> expected_outputs) {
     assert(inputs.size() == expected_outputs.size());
 
     for (int i=0; i<inputs.size(); i++) {
-        unsigned long long* input_ = inputs[i];
-        unsigned long long* expected_output_ = expected_outputs[i];
+        std::array<unsigned long long, SPONGE_WIDTH> input_ = inputs[i];
+        std::array<unsigned long long, SPONGE_WIDTH> expected_output_ = expected_outputs[i];
 
-        F input[SPONGE_WIDTH] = {F(0)};
+        std::array<F, SPONGE_WIDTH> input;
         for (int i=0; i<SPONGE_WIDTH; i++) {
             input[i] = F(input_[i]);
         }
 
-        F* output = poseidon(input);
+        std::array<F, SPONGE_WIDTH> output = poseidon(input);
 
         for (int j=0; j<SPONGE_WIDTH; j++) {
             F ex_output = F(expected_output_[j]);
@@ -102,15 +37,15 @@ void check_test_vectors(std::vector<unsigned long long*> inputs, std::vector<uns
 }
 
 void test_vectors() {
-    unsigned long long input0[SPONGE_WIDTH] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    unsigned long long expected_output0[SPONGE_WIDTH] = {
+    std::array<unsigned long long, SPONGE_WIDTH> input0 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::array<unsigned long long, SPONGE_WIDTH> expected_output0 = {
          0x3c18a9786cb0b359, 0xc4055e3364a246c3, 0x7953db0ab48808f4, 0xc71603f33a1144ca,
          0xd7709673896996dc, 0x46a84e87642f44ed, 0xd032648251ee0b3c, 0x1c687363b207df62,
          0xdf8565563e8045fe, 0x40f5b37ff4254dae, 0xd070f637b431067c, 0x1792b1c4342109d7
     };
 
-    std::vector<unsigned long long*> test_inputs = {input0};
-    std::vector<unsigned long long*> expected_outputs = {expected_output0};
+    std::vector<std::array<unsigned long long, SPONGE_WIDTH>> test_inputs = {input0};
+    std::vector<std::array<unsigned long long, SPONGE_WIDTH>> expected_outputs = {expected_output0};
 
     check_test_vectors(test_inputs, expected_outputs);
 }
